a81491c3f3d17cf8a3a59dc1d9b38ba9b5767a84
Paul <corolorn@gmail.com>
2017-07-13 22:13:09 +0300

    Create task041_Solution.java

---
+++ task041_Solution.java
@@ -0,0 +1,34 @@
+/* 
+День недели
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        String m = "";
+        switch (x) {
+            case 1: m = "понедельник";
+            break;
+            case 2: m = "вторник";
+            break;
+            case 3: m = "среда";
+            break;
+            case 4: m = "четверг";
+            break;
+            case 5: m = "пятница";
+            break;
+            case 6: m = "суббота";
+            break;
+            case 7: m = "воскресенье";
+            break;
+            default: m = "такого дня недели не существует";
+            break;
+        }
+        System.out.println(m);
+        
+    }
+}
