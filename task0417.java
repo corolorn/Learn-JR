122fa6dbf67b0ebc97b66eae4991f3fc0f1579ec
Paul <corolorn@gmail.com>
2017-07-14 19:57:19 +0300

    Create task0417.java
    Существует ли пара?
    Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
    Если такая пара существует, вывести на экран числа через пробел.
    Если все три числа равны между собой, то вывести все три.
    
    Пример для чисел 1 2 2:
    2 2
    
    Пример для чисел 2 2 2:
    2 2 2

---
+++ task0417.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task04.task0417;
+
+/* 
+Существует ли пара?
+*/
+
+import java.io.*;
+
+public class task0417 {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a  = Integer.parseInt(br.readLine());
+        int b  = Integer.parseInt(br.readLine());
+        int c  = Integer.parseInt(br.readLine());
+        if(a==b && b==c){
+            System.out.println(a+" "+b + " " +c);}
+            else if (a==b){
+                System.out.println(a+" "+b );
+            }else if (b==c){
+                System.out.println(b+" "+c );
+            }else if (a==c){
+                System.out.println(a+" "+c );}
+            else ;
+            
+        
+
+    }
+}
